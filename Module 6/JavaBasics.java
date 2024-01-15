3511674e3fd6fad06bd0b0c6954c8c0a589cc85b
Ankita-j-patil <ankitajagdishpatil172003@gmail.com>
2024-01-15 18:47:23 +0530

    M6 first commit

---
+++ Module 6/JavaBasics.java
@@ -0,0 +1,68 @@
+import java.util.*;
+public class JavaBasics{
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        // String input = s.next();
+        // System.out.println(input);
+
+        // String name = s.nextLine();
+        // System.out.println(name);
+
+
+        
+       
+
+
+        int number =s.nextInt();
+        System.out.println(number);
+
+        // float price = s.nextFloat();
+        // System.out.println(price);
+
+        // System.out.print("Hello Ankita!\n");
+        //  System.out.print("Hello Ankita!\n");
+        //   System.out.print("Hello Ankita!\n");
+
+        //   System.out.println("****");
+        //   System.out.println("***");
+        //   System.out.println("**");
+        //   System.out.println("*");
+
+        //   int a = 4;
+        //   int b = 3;
+        //   String name = "Ankita Patil";
+        //   System.out.println(a);
+        //   System.out.println(b);
+        //   System.out.println(name);
+
+
+        // //   Data Types
+        // byte c = 8;
+        // System.out.println(c); 
+
+        // char d = 'R';
+        // System.out.println(d);
+
+        // boolean var = true;
+        // System.out.println(var);
+
+        // // float price = 0.5;
+        // // System.out.println(price);
+
+        // int number = 23;
+        // System.out.println(number);
+
+        // short n = 123;
+        // System.out.println(n);
+
+
+
+        // sum of val1 & val2
+        // int val1 = 2;
+        // int val2 = 4;
+        // int sum = val1+val2;
+        // System.out.println(sum);
+        s.close();
+
+    }
+}
---
+++ Module 6/Sum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Sum {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        
+        int number1 = sc.nextInt();
+        int number2 = sc.nextInt();
+       
+        int result = number1 + number2 ;
+        System.out.println(result);
+        sc.close();
+        
+    }
+   
+}
