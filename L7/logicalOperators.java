8286d53fc27ae08895d6f6d99891ba8e5d0317a8
Ankita-j-patil <ankitajagdishpatil172003@gmail.com>
2024-01-16 23:45:14 +0530

    L7 second commit

---
+++ L7/logicalOperators.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class logicalOperators {
+    public static void main (String args[]){
+    //    Logical AND
+        System.out.println( (4>2) && (5>9));
+
+    //     Logical OR
+        System.out.println( (3>4) || (8>5));
+
+    // Logical NOT
+        System.out.println(!(6>9));
+    }
+}
---
+++ L7/relationalOperators.java
@@ -0,0 +1,25 @@
+
+
+public class relationalOperators {
+    public static void main(String args[]){
+        int a = 10;
+        int b = 10;
+        //  == operator
+       System.out.println(a == b);
+
+    //    != operator
+    System.out.println(a != b);
+
+    // > operator
+    System.out.println(a > b);
+
+    // < operator
+    System.out.println(a < b);
+
+    // >= operator
+    System.out.println(a <= b);
+
+    // <= operator
+    System.out.println(a <= b);
+    }
+}
