84ae7ead6641e67317f22acfa7d9ae305f94060d
Ankita-j-patil <ankitajagdishpatil172003@gmail.com>
2024-01-17 13:04:35 +0530

    L7 final commit

---
+++ L7/assignmentOperators.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class assignmentOperators {
+    public static void main(String args[]){
+        int a = 10;
+        // a = a + 10;
+        a += 10;
+
+        int b =4;
+        // b = b - 2;
+        b -=2;
+
+        int c = 5;
+        // c=c*4;
+        c*= 4;
+
+        int d = 10;
+        // d = d/5;
+        d/= 5;
+         System.out.println(a);
+         System.out.println(b);
+         System.out.println(c);
+         System.out.println(d);
+    }
+}
