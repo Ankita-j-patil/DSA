85e5d7a461fcefbe30adb076b27f99e0e3aa8f95
Ankita-j-patil <ankitajagdishpatil172003@gmail.com>
2024-01-26 22:47:45 +0530

    Functions first commit

---
+++ Functions/functionDemo.java
@@ -0,0 +1,13 @@
+public class functionDemo {
+
+    public static void printHelloWorld(){
+        System.out.println("Hello World");
+        
+    }
+    
+    
+    public static void main(String args[]){
+        printHelloWorld();  
+        // function call
+    }
+}
