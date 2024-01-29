76dd39decfee9b7ab1f36a6510183e413c75917b
Ankita-j-patil <ankitajagdishpatil172003@gmail.com>
2024-01-30 00:07:02 +0530

    Functions fifth commit

---
+++ Functions/functionOverloading.java
@@ -0,0 +1,23 @@
+// function overloading with the parameters
+
+import java.util.*;
+public class functionOverloading {
+    public static int sum(int a,int b){
+        return a+b;
+    }
+
+    public static int sum(int a,int b,int c){
+            return a+b+c;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a : ");
+        int a =sc.nextInt();
+        System.out.println("Enter b : ");
+        int b =sc.nextInt();
+        System.out.println("Enter c : ");
+        int c =sc.nextInt();
+        System.out.println(sum(a,b));
+        System.out.println(sum(a,b,c));
+    }
+}
