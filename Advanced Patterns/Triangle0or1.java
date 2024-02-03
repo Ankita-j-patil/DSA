3f2396b250438d798ca328d93ad12e3db74071da
Ankita-j-patil <ankitajagdishpatil172003@gmail.com>
2024-02-03 18:18:36 +0530

    Arrays first commit

--- Advanced Patterns/Triangle0or1.java
+++ Advanced Patterns/Triangle0or1.java
@@ -4,11 +4,8 @@ public class Triangle0or1 {
     public static void triangle(int n){
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
-                if(i==j){
+                if((i+j)%2==0){
                     System.out.print(1);
-                }
-                else if(j<i){
-                    System.out.print(0);
                 }
                 else{
                     System.out.print(0);
