79d4489dd60cacfcf53043c9509378e0862abde2
Ankita-j-patil <ankitajagdishpatil172003@gmail.com>
2024-03-04 20:02:17 +0530

    Strings : Introduction

---
+++ Strings/indroduction.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class indroduction {
+    public static void main(String args[]){
+        char arr[] = {'a','b','c'};
+        String str1 = "abcd";
+        String str = new String("xyz");
+
+        Scanner sc = new Scanner(System.in);
+        String name;
+        name = sc.nextLine();
+        System.out.println(name);
+    }
+}
