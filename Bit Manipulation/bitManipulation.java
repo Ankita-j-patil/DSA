c0a3f93e43638484d9eaa419cb091971bc7dfd11
Ankita-j-patil <ankitajagdishpatil172003@gmail.com>
2024-03-16 23:08:15 +0530

    Bit Manipulation : Bitwise operators

--- Bit Manipulation/bitManipulation.java
+++ Bit Manipulation/bitManipulation.java
@@ -2,6 +2,14 @@
 
 public class bitManipulation {
     public static void main(String args[]){
-        
+        // Binary AND (&)
+        System.out.println(2&3);
+
+        // Binary OR(|)
+        System.out.println(5|6);
+
+        // Binary XOR(^)
+        System.out.println(5^6);
+
     }
 }
