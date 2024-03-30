3f13af044cbe9384e7675730d0c6f8cc0f964f41
Ankita-j-patil <ankitajagdishpatil172003@gmail.com>
2024-03-30 18:45:40 +0530

    Bit Manipulation :  update ith bit updated1

--- Bit Manipulation/updateIthBit.java
+++ Bit Manipulation/updateIthBit.java
@@ -1,3 +1,30 @@
 public class updateIthBit {
     // update ith bit
+    public static int ClearIthBit(int n,int i){
+        int bitMask = ~(1<<i);
+        return n & bitMask;
+    }
+    public static int SetIthBit(int n,int i){
+        int bitMask = 1<<i;
+        return n|bitMask;
+    }
+    public static int UpdateIthBit(int n,int i,int newBit){
+        if(newBit == 0){
+            return ClearIthBit(n,i);
+        }
+        else{
+           return SetIthBit(n,i);
+        }
+    }
+
+    // Another approach
+    public static int UpdateIthBit1(int n,int i,int newBit){
+       n = ClearIthBit(n,i);
+       int BitMask = newBit<<i;
+       return n | BitMask;
+    }
+
+    public static void main(String args[]){
+        System.out.println(UpdateIthBit1(10,2,1));
+    }
 }
