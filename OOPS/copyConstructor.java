6a912c7655d6e1a18a8f619491116d38437bc46c
Ankita-j-patil <ankitajagdishpatil172003@gmail.com>
2024-06-07 18:50:53 +0530

    OOPS :Deep & Shallow Constructors

--- OOPS/copyConstructor.java
+++ OOPS/copyConstructor.java
@@ -4,14 +4,15 @@ public class copyConstructor {
         s1.name = "Ankita";
         s1.roll = 38;
         s1.password = "xyz"; 
+        s1.marks[0] = 100;
+        s1.marks[1] = 90;
+        s1.marks[2] = 80;
 
         Student s2 = new Student(s1);
         s2.password = "abc";
         s1.marks[2] =100;   
 
-        s1.marks[0] = 100;
-        s1.marks[1] = 90;
-        s1.marks[2] = 80;
+        
 
         for(int i=0;i<3;i++){
             System.out.println(s2.marks[i]);
@@ -26,12 +27,22 @@ class Student{
     String password;
     int marks[];
 
-    // Copy Constructor
+    // Shallow Copy Constructor
+    // Student(Student s1){
+    //     marks = new int[3];
+    //     this.name = name;
+    //     this.roll = roll;
+    //     this.marks = s1.marks;
+    // }
+
+    // Deep copy constructor
     Student(Student s1){
         marks = new int[3];
         this.name = name;
         this.roll = roll;
-        this.marks = s1.marks;
+        for(int i=0;i<marks.length;i++){
+             this.marks[i] = s1.marks[i]; 
+        }
     }
     
     Student(){
