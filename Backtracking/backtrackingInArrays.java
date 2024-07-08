f0da5fd3319a2d07c88a56e9e2050b6edec4b6d9
Ankita-j-patil <ankitajagdishpatil172003@gmail.com>
2024-07-08 20:32:29 +0530

    BackTracking in arrays

---
+++ Backtracking/backtrackingInArrays.java
@@ -0,0 +1,26 @@
+public class backtrackingInArrays {
+    public static void changeArr(int arr[],int i,int val){
+        // base cse
+        if(i == arr.length){
+            printArray(arr);
+            return;
+        }
+
+        // recursion
+        arr[i] = val;
+        changeArr(arr,i+1,val+1);
+        arr[i] = arr[i] - 2;//backtracking step
+    }
+
+    public static void printArray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print (arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+        int arr[] = new int[5];
+        changeArr(arr,0,1);
+        printArray(arr);
+    }
+}
