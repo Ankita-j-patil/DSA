5d6c8afa5a26d5f385cd02ce389eccb34adf821f
Ankita-j-patil <ankitajagdishpatil172003@gmail.com>
2024-07-19 22:37:10 +0530

    Multi-Dimensional ArrayList

---
+++ ArrayLists/tempCodeRunnerFile.java
@@ -0,0 +1,8 @@
+ // for(int i=0;i<mainList.size();i++){
+        //     ArrayList<Integer> currList = mainList.get(i);
+        //     for(int j=0;j<currList.size();j++){
+        //         System.out.print(currList.get(j) + " ");
+        //     }
+        //     System.out.println();
+        // }
+        // System.out.println(mainList);
