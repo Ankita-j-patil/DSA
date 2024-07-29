37ac984e9faf7650047c8b7df44f02c259122747
Ankita-j-patil <ankitajagdishpatil172003@gmail.com>
2024-07-29 20:48:31 +0530

    add last node in Linked List

--- Linked List/addNodeInLinkedList.java
+++ Linked List/addNodeInLinkedList.java
@@ -29,10 +29,24 @@ public class addNodeInLinkedList {
         head = newNode;
     }
 
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+
+        tail.next = newNode;
+
+        tail = newNode;
+    }
+
     public static void main(String args[]){
         addNodeInLinkedList ll = new addNodeInLinkedList();
         ll.addFirst(1);
         ll.addFirst(2);
+        ll.addLast(3);
+        ll.addLast(4);
        
 }
 }
