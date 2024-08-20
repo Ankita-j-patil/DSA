f6ea8e575a2bbb4ea262157382f639e247ea3627
Ankita-j-patil <ankitajagdishpatil172003@gmail.com>
2024-08-20 13:22:48 +0530

    Recursive search - create addFirst function

--- Linked List/recursiveSearch.java
+++ Linked List/recursiveSearch.java
@@ -10,6 +10,22 @@ public class recursiveSearch {
     }
     public static Node head;
     public static Node tail;
+
+    public void addFirst(int data){
+        
+        Node newNode = new Node(data);
+       
+        if(head == null){
+            head = tail = newNode;
+            return;
+        }
+
+        
+        newNode.next = head; //link
+
+        // step3 head = newNode
+        head = newNode;
+    }
     public static void main(String[] args) {
         
     }
