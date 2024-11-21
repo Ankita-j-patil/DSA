ab8f4cb21a3067c62d70e46e87bb5b7c463ab9bf
Ankita-j-patil <ankitajagdishpatil172003@gmail.com>
2024-11-21 11:36:55 +0530

    Question:Duplicate Parentheses

---
+++ Stack/duplicateParentheses.java
@@ -0,0 +1,31 @@
+// Time Complexity : O(n)
+import java.util.*;
+public class duplicateParentheses {
+    public static boolean isDuplicate(String str){
+        Stack<Character> s = new Stack<>();
+        for(int i=0;i<str.length();i++){
+            char ch = str.charAt(i);
+            if(ch==')'){
+                int cnt = 0;
+            while(s.peek()!='('){
+                s.pop();
+                cnt++;
+            }
+            if(cnt<1){
+                return true;
+            }
+            s.pop();
+            }
+            else{
+                s.push(ch);
+            }
+        }
+        return false;
+    }
+    public static void main(String args[]){
+        String str1 ="((a+b))";
+        String str2 = "((a+b)-(c*d))";
+        System.out.println(isDuplicate(str1));
+        System.out.println(isDuplicate(str2)); 
+    }
+}
